940139d6de6882ae5dad8bf662e36d922f8781f0
fakuivan <fakuivan@gmail.com>
2024-01-30 10:10:12 -0300

    Add utils header file

---
+++ includes/logic/utils.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <functional>
+
+namespace fk_esphome {
+namespace utils {
+template <typename... Args>
+class UpdateChannel {
+ public:
+  UpdateChannel(update_func_t &&update_func) {}
+  bool operator()(Args... &&args) {
+    bool last_update_not_handled = waiting_for_response;
+    waiting_for_response = true;
+    update_func([this]() { this->waiting_for_response = false; },
+                std::forward<Args>(args)...);
+    return last_update_not_handled;
+  }
+
+  bool check_update() {
+    if (!waiting_for_response) {
+      return false;
+    }
+    waiting_for_response = false;
+    return true;
+  }
+
+  bool do_if(action_t &&action) {
+    if (!check_update()) {
+      return false;
+    }
+    action();
+    return true;
+  }
+
+ private:
+  using action_t = std::function<void()>;
+  using cancel_update_t = std::function<void()>;
+  using update_func_t = std::function<void(cancel_update_t &&, Args...)>;
+  bool waiting_for_response;
+};
+
+class UpdateChannelGuard {
+  template <typename... Args>
+  UpdateChannelGuard(UpdateChannel<Args...> &chann)
+      : has_update(chann.check_update()) {}
+  operator bool() const { return has_update; }
+  bool has_update = false;
+};
+}  // namespace utils
+}
